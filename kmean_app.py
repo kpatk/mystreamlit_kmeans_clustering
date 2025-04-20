eab176a2cb32fb34905a28d6ecf7ef5148a19c30
kpatk <38811803+kpatk@users.noreply.github.com>
2025-04-20 16:37:15 +0700

    Add files via upload

---
+++ kmean_app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 16:14:22 2025
+
+@author: patzy
+"""
+
+import streamlit as st
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+
+#load model
+with open('kmeans_model.pkl','rb') as f:
+    loaded_model = pickle.load(f)
+    
+# Set the app title
+st.title("K=Means Clustering Visualizer by Khwunta Kirimasthong")
+
+#Set the page config
+st.set_page_config(page_title = "K-Means Clustering", layout="centered")
+
+#Load dataset
+X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
+
+#predicr using the loaded model
+y_kmeans = loaded_model.predict(X)
+
+#plotting
+fig, ax = plt.subplots()
+scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
+ax.set_title('k-Means Clustering')
+ax.legend()
+st.pyplot(fig)
+
+
+
